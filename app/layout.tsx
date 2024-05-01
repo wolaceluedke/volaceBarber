a8901688c1f9633df18077bc9532076256640d93
Wolace Luedke <wolacepin@hotmail.com>
2024-05-01 08:13:20 -0300

    feat: add toast on booking sucess

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./_components/footer";
 import AuthProvider from "./_providers/auth";
+import { Toaster } from "./_components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} dark`}>
         <AuthProvider>
-        {children}
-        <Footer />
+          {children}
+          <Toaster />
+          <Footer />
         </AuthProvider>
       </body>
     </html>
